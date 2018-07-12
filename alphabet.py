112b050dbc2f57e5e3a3ce9228e545bc62ad5bd1
Monagitme <41141005+Monagitme@users.noreply.github.com>
2018-07-12 14:56:00 +0530

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,10 @@
+n = input()
+n = n.lower()
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+if(n == 'd'):
+   print("yes")
+elif(n in alphabet):
+   print("Alphabet")
+else:
+   print("No")
+   
